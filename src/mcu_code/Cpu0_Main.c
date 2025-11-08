5b484cf5a3be7e8c030db4f7de57f0ae8b2cf729
DESKTOP-CMUG66K\LMH <valsgml@naver.com>
2025-11-08 23:48:27 +0900

    backup for portfolio

---
+++ src/mcu_code/Cpu0_Main.c
@@ -0,0 +1,26 @@
+/**********************************************************************************************************************
+ * file Cpu0_Main.c
+ * copyright Copyright (C) Infineon Technologies AG 2019
+ * Boost Software License - Version 1.0 - August 17th, 2003
+ *********************************************************************************************************************/
+#include "Cpu0_Main.h"
+
+void core0_main(void)
+{
+    System_Init();
+
+    volatile int parking_flag = 0;
+
+    while(1)
+    {
+        parking_flag = Can_Get_Parking();
+
+        if (parking_flag == 1)
+        {
+            Parking_On();
+
+            parking_flag = 0;
+            Can_Let_Parking(parking_flag);
+        }
+    }
+}
---
+++ src/mcu_code/Cpu0_Main.h
@@ -0,0 +1,11 @@
+/**********************************************************************************************************************
+ * file Cpu0_Main.h
+ * copyright Copyright (C) Infineon Technologies AG 2019
+ * Boost Software License - Version 1.0 - August 17th, 2003
+ *********************************************************************************************************************/
+#ifndef CPU0_MAIN_H_
+#define CPU0_MAIN_H_
+
+#include "Sys_Init.h"
+
+#endif /* CPU0_MAIN_H_ */
